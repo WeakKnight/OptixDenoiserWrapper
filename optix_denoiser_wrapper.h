326067fadd9f7f0c239597b5f135b10aa6f5c053
hermitli <hermitli@tencent.com>
2021-06-24 21:05:18 +0800

    Init

---
+++ optix_denoiser_wrapper.h
@@ -0,0 +1,8 @@
+#pragma once
+#define OPTIX_DENOISER_WRAPPER_API __declspec(dllexport) 
+
+extern "C" 
+{
+    OPTIX_DENOISER_WRAPPER_API void test_val();
+}
+
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "optix_denoiser_wrapper.h"
+// #include <cuda_runtime.h>
+// #include <optix.h>
+// #include <optix_function_table_definition.h>
+// #include <optix_stubs.h>
+
+int main()
+{
+	// auto res = optixInit();
+	// int a = 1;
+   test_val();
+}
